8a57b917edc1d129935008820e23d54b6ef94d6f
Jordan Yuan <yuanjor93@gmail.com>
2017-01-26 02:27:58 +0000

    /intergrated bower and boot strap, edited index html to use global jquery and css

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -2,6 +2,19 @@ var express = require('express');
 var app = express();
 
 var port = process.env.PORT;
+
+app.use(express.static('public'));
+app.use(express.static('src/views'));
+app.use(express.static('bower_components'));
+
+app.get('/', function(req, res){
+    res.send('hello world');
+});
+
+app.get('/routing', function(req, res){
+    res.send('I am a cheesy potato');
+});
+
 app.listen(port, function(err){
    console.log('The server is running on port:  ' + port);
     
