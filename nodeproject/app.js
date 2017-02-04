dd1835989831b0fdd69778c0110827b7bbdf96f6
Jordan Yuan <yuanjor93@gmail.com>
2017-02-04 22:11:40 +0000

    /removed index.html and added ejs + index.ejs to manage HTML

--- nodeproject/app.js
+++ nodeproject/app.js
@@ -4,18 +4,24 @@ var app = express();
 var port = process.env.PORT;
 
 app.use(express.static('public'));
-app.use(express.static('src/views'));
 app.use(express.static('bower_components'));
 
+app.set('views','./src/views');    
+app.set('view engine','ejs');    
+
+
 app.get('/', function(req, res){
-    res.send('hello world');
+   // res.send('hello world');
+    res.render('index', {list: ['first var', '2', '3rd v@l']});
+    
 });
 
 app.get('/routing', function(req, res){
-    res.send('I am a cheesy potato');
+ res.send('I am a cheesy potato');
+    
 });
-
+ 
 app.listen(port, function(err){
    console.log('The server is running on port:  ' + port);
-    
+
 });
